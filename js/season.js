eb232e78be810149e698831e09d92bf7c10ef732
jeonghwa,park <pjh445@gmail.com>
2023-08-29 16:11:14 +0900

    Update season.js

--- js/season.js
+++ js/season.js
@@ -8,16 +8,16 @@ $(document).ready(function(){
 	z2.css("background","blue");
 	
 	//링크이동한 페이지로 인수 전달하기*
-	var urlSearch = new URLSearchParams(location.search);
-       	var param = urlSearch.get('p');
+	const pa = $(location).prop("search");//?쿼리스트링
+	console.log(pa); //?p=0
+	const i = pa.substr(-1);// 0,1,2,3
+	console.log( i );  //1,2,3 중 하나.
 	
 	//index페이지는 ?쿼리스트링이 없는데 빈문자열로 인식하는 문제
-	if(param  == 0){		
-		console.log("11");
-		$("nav a").addClass("act");
-	}
-	
-	
+	if( i != ""){		
+		$("nav a").eq( i ).addClass("act");
+	}	
+
 		
 	
 });////////////전체끝
