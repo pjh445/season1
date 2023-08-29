d9c88618cb252a19c8f1a491915a2e9a9a037f54
jeonghwa,park <pjh445@gmail.com>
2023-08-29 20:32:38 +0900

    Add files via upload

--- js/season.js
+++ js/season.js
@@ -15,11 +15,11 @@ $(document).ready(function(){
 	
 	//index페이지는 ?쿼리스트링이 없는데 빈문자열로 인식하는 문제
 	if( i != ""){		
-		$("#menu", parent.document ).contents().find("nav a").eq( i ).addClass("act");
-		$(top.document).find("#$nav").attr("src","orange");		
+		//$("#menu", parent.document ).contents().find("nav a").eq( i ).addClass("act");
+		//$(top.document).find("#$nav").attr("src","orange");		
 		$("#menu", parent.document ).contents().find("nav a").addClass("act");
 	}
-	
+	$('#menu').contents().find('nav a').css("background","red");
 	$("#menu", parent.document ).contents().find("nav a").css("background","red");
 	$(top.document).find("#menu").contents().find("nav a").attr("src","orange");	
 	
