4529d3363430e8ea44b81dcb881021fa8e642f18
jeonghwa,park <pjh445@gmail.com>
2023-08-29 22:00:31 +0900

    Add files via upload

--- js/season.js
+++ js/season.js
@@ -1,21 +1,9 @@
 $(document).ready(function(){	
 
-    $("nav").load("nav.html");
-
-		
-		
-	//$('#부모페이지내다른iframe의노드id', parent.frames["iframeid"].document).css('background', 'black');
-	//$("#myid", top.document); 
-	//$("#myid", parent.document.body); 
-	//$(elementid,window.parent.frames[0].document).attr(attributeName)
-
-	//$("nav a").eq(1).css('background', 'black');	
-	//$('#menu', parent.document).attr('class' , 'act');
-	
+    $("#menu").load("/nav.html");	
 	
 	//링크이동한 페이지로 인수 전달하기*	
-	//$("#myid", top.document);
-	const pa = $(location,window.parent).prop("search");//?쿼리스트링
+	const pa = $(location).prop("search");//?쿼리스트링
 	console.log(pa); //?p=0
 	const i = pa.substr(-1);// 0,1,2,3
 	console.log( i );  //1,2,3 중 하나.
@@ -26,7 +14,6 @@ $(document).ready(function(){
 	}
 	
 });////////////전체끝
-
 
 
 
