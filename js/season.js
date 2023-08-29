3407bc383d435019419b9aa6d9756240c47440cb
jeonghwa,park <pjh445@gmail.com>
2023-08-29 21:54:54 +0900

    Add files via upload

--- js/season.js
+++ js/season.js
@@ -1,37 +1,32 @@
 $(document).ready(function(){	
+
+    $("nav").load("nav.html");
+
 		
-	let xx = $("embed").contents();
-	let yy = xx.find("a").eq(0);
-	yy.css("background", 'red');
-	let z1 = $("object").contents();
-	let z2 = z1.find("header");
-	z2.css("background","blue");
+		
+	//$('#부모페이지내다른iframe의노드id', parent.frames["iframeid"].document).css('background', 'black');
+	//$("#myid", top.document); 
+	//$("#myid", parent.document.body); 
+	//$(elementid,window.parent.frames[0].document).attr(attributeName)
+
+	//$("nav a").eq(1).css('background', 'black');	
+	//$('#menu', parent.document).attr('class' , 'act');
 	
-	//링크이동한 페이지로 인수 전달하기*
-	const pa = $(location).prop("search");//?쿼리스트링
+	
+	//링크이동한 페이지로 인수 전달하기*	
+	//$("#myid", top.document);
+	const pa = $(location,window.parent).prop("search");//?쿼리스트링
 	console.log(pa); //?p=0
 	const i = pa.substr(-1);// 0,1,2,3
 	console.log( i );  //1,2,3 중 하나.
 	
 	//index페이지는 ?쿼리스트링이 없는데 빈문자열로 인식하는 문제
 	if( i != ""){		
-		$("#menu", parent.document ).contents().find("nav a").eq( i ).addClass("act");
-		$(top.document).find("#menu").contents().find("nav a").addClass("act");
-		$("#menu", parent.document ).contents().find("nav a").addClass("act");
+		$("nav a").eq( i ).addClass("act");
 	}
-	$('#menu').contents().find('nav a').css("background","red");
-	$("#menu", parent.document ).contents().find("nav a").css("background","red");
-	$(top.document).find("#menu").contents().find("nav a").addClass("act");
-	
-	console.log(  $('#menu').contents().find('nav a').eq(2).text()  );
-	
-	//$('#부모페이지내다른iframe의노드id', parent.frames["iframeid"].document).css('background', 'black');
-	//$("#myid", top.document); 
-	
-	$("#menu", top.document).contents().find('nav a').eq(2).text();
-	$("nav a").eq(1).css('background', 'black');
 	
 });////////////전체끝
+
 
 
 
