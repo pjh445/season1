b7b34b0863559bbd1eeafbfe4e6dce0d8fae1107
jeonghwa,park <pjh445@gmail.com>
2023-08-30 14:19:16 +0900

    Update season.js

--- js/season.js
+++ js/season.js
@@ -6,9 +6,9 @@ $(document).ready(function(){
 
 	
 	//링크이동한 페이지로 인수 전달하기	
-	const pa = $(location).prop("search");//?쿼리스트링
+	let pa = $(location).prop("search");//?쿼리스트링
 	console.log(pa); //?p=0
-	const i = pa.substr(-1);// 0,1,2,3
+	let i = pa.substr(-1);// 0,1,2,3
 	console.log( i );  //1,2,3 중 하나.
 	
 	//index페이지는 ?쿼리스트링이 없는데 빈문자열로 인식하는 문제
