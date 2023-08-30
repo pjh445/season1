50d729d08e3669d20fbad6920adf37d171706e93
jeonghwa,park <pjh445@gmail.com>
2023-08-30 16:10:33 +0900

    Update season.js

--- js/season.js
+++ js/season.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	
 	$("#menu").load(function(){	
-		let txt = $(this).contents().find("a").html();
+		let txt = $(this).contents().find("nav").html();
 		console.log(txt);
 		$(this).contents().find("nav").css("background","red");			
 	});
