9aa3d9f862899ec38523474771f56c0936567267
jeonghwa,park <pjh445@gmail.com>
2023-08-30 21:28:25 +0900

    Update test.js

--- js/test.js
+++ js/test.js
@@ -4,14 +4,9 @@ $(document).ready(function(){
 	const pa = $(location).attr("search");//?post=123 (쿼리스트링)
 	console.log(pa); //?p=0
 	const i = pa.substr(-1);// 0,1,2,3
-	console.log( i );  //1,2,3 중 하나.	
+	console.log( i );  //1,2,3 중 하나.
 	
-	$("#menu").load(function(){	
-		let txt = $(this).contents().find("nav").html();
-		console.log(txt);
-		//$(this).contents().find("nav a").eq(1).css("background","red");		
-		//$(this).contents().find("nav a").eq(2).addClass("act");	
-		//index페이지는 ?쿼리스트링이 없는데 빈문자열로 인식하는 문제
+	$("#menu").load(function(){
 		if( i != ""){		
 			$("#menu").contents().find("nav a").eq(i).addClass("act");
 		}		
