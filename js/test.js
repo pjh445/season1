715488c44fc1285fbe951e242d9399651f6e4320
jeonghwa,park <pjh445@gmail.com>
2023-08-30 15:24:34 +0900

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
+	/*
 	$("#myframe").load(function(){	
 		var txt = $(this).contents().find("div").html();
 		console.log(txt);
 		$(this).contents().find("div").css("background","red");			
 	});	
+	*/
+	
+		let txt = $("#myframe").contents().find("div").html();
+		console.log(txt);
+		$(this).contents().find("div").css("background","red");			
+	
 });
