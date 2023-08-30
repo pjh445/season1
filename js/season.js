7ba9c782a47bd399d6a879c7460d3b3568f12b87
jeonghwa,park <pjh445@gmail.com>
2023-08-30 14:17:46 +0900

    Update season.js

--- js/season.js
+++ js/season.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){	
 
-    $("#menu").load("nav.html");
+	$("#menu").load("nav.html");
   
-    let txt = $("#menu").contents().find("a").html();
-	console.log(txt);
+    	console.log(txt);
 
 	
 	//링크이동한 페이지로 인수 전달하기	
@@ -13,13 +12,11 @@ $(document).ready(function(){
 	console.log( i );  //1,2,3 중 하나.
 	
 	//index페이지는 ?쿼리스트링이 없는데 빈문자열로 인식하는 문제
-	if( i != ""){
-	   // $("#menu").load(function(){		
-		$("#menu").contents().find("a").eq(i).attr("class", "act");		
-   	    //});
+	if( i != ""){				
+		$("#menu").contents().find("a").eq(i).attr("class", "act");
 	}
 	
-});////////////전체끝
+});////////////////////////전체끝
 
 
 
