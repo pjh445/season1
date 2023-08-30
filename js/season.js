eb5d651a64419fcba200dcf572253e5840219fdb
jeonghwa,park <pjh445@gmail.com>
2023-08-30 19:44:13 +0900

    Add files via upload

--- js/season.js
+++ js/season.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 	console.log( x );
 	
 	/*링크이동한 페이지로 인수 전달하기	
-	const pa = $(location).prop("search");//?쿼리스트링
+	const pa = $(location).attr("search");//?post=123 (쿼리스트링)
 	console.log(pa); //?p=0
 	const i = pa.substr(-1);// 0,1,2,3
 	console.log( i );  //1,2,3 중 하나.
